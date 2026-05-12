9c1a9e16f19764c748bc994d46a58d57085fe619
ananyaghosal5922-bit <ananyaghosal5922@gmail.com>
2026-05-13 00:06:36 +0530

    Added answer selection

--- src/pages/QuizApp.jsx
+++ src/pages/QuizApp.jsx
@@ -1,17 +1,50 @@
+import { useState } from "react";
 import questions from "../data/questions";
 
 function QuizApp() {
+
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+
+  const [selectedAnswer, setSelectedAnswer] = useState("");
+
+  const nextQuestion = () => {
+    setCurrentQuestion(currentQuestion + 1);
+
+     setSelectedAnswer("");
+  };
+
   return (
     <div>
+
       <h1>Quiz App</h1>
 
-      <h2>{questions[0].question}</h2>
+      <h2>{questions[currentQuestion].question}</h2>
 
-      {questions[0].options.map((option, index) => (
-        <button key={index}>
-          {option}
-        </button>
+      {questions[currentQuestion].options.map((option, index) => (
+        <div key={index}>
+          <button onClick={() => setSelectedAnswer(option)}
+
+            style={{
+              backgroundColor:
+                selectedAnswer === option ? "lightgreen" : "white",
+    color: "black",
+    padding: "10px",
+    margin: "5px",
+    width: "200px",
+    borderRadius: "5px",
+    border: "none",
+    cursor: "pointer",
+            }}>{option}</button>
+        </div>
       ))}
+           <br />
+
+      {currentQuestion < questions.length - 1 && (
+        <button onClick={nextQuestion}>
+          Next
+        </button>
+      )}
+
     </div>
   );
 }
